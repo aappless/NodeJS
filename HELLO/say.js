eadcd6ee90fecaef0b3254887a1643b66df7f729
aappless <aappless@gmail.com>
2018-09-02 09:57:32 +0800

    FUNCTION

---
+++ HELLO/say.js
@@ -0,0 +1,12 @@
+// 函式
+function sayhi(){
+    console.log("HI");
+}
+sayhi();
+
+function saybye(){
+    console.log("BYE");
+}
+saybye();
+function sayHELLO(){console.log("hello");}
+sayHELLO(); //沒加括號 會無做用;
